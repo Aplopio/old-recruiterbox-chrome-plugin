33f0250028c4660a9f7ccff55c794fe1effdb8a9
Sushant Gupta <sushant711@gmail.com>
2014-08-28 15:12:19 +0530

    Setting up Grunt task, and other boiler stuff

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    // Configuration goes here
+    grunt.initConfig({
+        jst: {
+            compile: {
+                options: {
+                    // prettify: true,
+                    templateSettings: {
+                        variable: 'rbp'
+                    },
+                    processName: function( full_filename ) {
+                        var filename = full_filename.match(/\/[^\/]*$/g)[0].substr(1);
+                        return 'jst-' + filename.replace(/\.html$/, '');
+                    },
+                    namespace: '_RBP.JST'
+                },
+                files: {
+                    "templates.js": ["jst/*.html"]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jst');
+    grunt.registerTask('default', ['jst']);
+};
